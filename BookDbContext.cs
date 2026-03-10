2e0a62e66b7d836d408634c070de32e98b993b1b
Min Naing Tun <minnaingtun.pro@gmail.com>
2026-03-10 11:06:09 +0630

    Add models and dbContext.

---
+++ BookDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookDbApi.DbModels;
+
+namespace BookDbApi
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) { }
+
+        public DbSet<Authors> Authors => Set<Authors>();
+        public DbSet<Categories> Categories => Set<Categories>();
+        public DbSet<Publishers> Publishers => Set<Publishers>();
+        public DbSet<Books> Books => Set<Books>();
+    }
+}
---
+++ BookDbModel.cs
@@ -0,0 +1,36 @@
+﻿namespace BookDbApi.DbModels
+{
+    public class Authors
+    {
+        public int AuthorID { get; set; }
+        public string? AuthorName { get; set; }
+        public string? AuthorBiography { get; set; }
+        public DateOnly AuthorBirthDate { get; set; }
+    }
+
+    public class Categories
+    {
+        public int CategoryID { get; set; }
+        public string? CategoryName { get; set; }
+        public string? CategoryDescription { get; set; }
+    }
+
+    public class Publishers
+    {
+        public int PublisherID { get; set; }
+        public string? PublisherName { get; set; }
+        public string? PublisherAddress { get; set; }
+        public string? PublisherWebsite { get; set; }
+    }
+
+    public class Books
+    {
+        public int BookID { get; set; }
+        public string? BookTitle { get; set; }
+        public int AuthorID { get; set; }
+        public int PublisherID { get; set; }
+        public int CategoryID { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
